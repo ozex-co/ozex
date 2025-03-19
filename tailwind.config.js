4f41ed05d7addbf38eef12444897a68b91c412a5
ozex-co <ozex.me.11@gmail.com>
2025-03-20 01:00:31 +0200

    solving problems

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js,vue}"], // تأكد من تعديل المسار حسب مشروعك
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
